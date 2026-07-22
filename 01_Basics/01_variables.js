b4282c405234cdcdafd65f7e87d10ae32f4957f2
achitrans <achitrans277@gmail.com>
2026-07-22 11:31:25 +0530

    initial commit

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,17 @@
+const collage="LPU"
+let name="Abhilasha"
+let age=22
+let semester=8
+
+console.log(collage);
+console.log(name);
+console.log(age);
+console.log(semester);
+
+/*
+- we can not redeclare or reassign the const.Once declaration
+- we can reassign the let but not redeclare.
+- in Modern Javascript we dont use var becuse var has behaviors (like function scope and hoisting) that often lead to bugs
+*/
+
+
